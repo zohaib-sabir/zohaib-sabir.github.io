8139d3de6b782c5d5ecd952f5ddf64a60393afea
Zohaib Sabir <zohaib.zaibi.pro@gmail.com>
2025-08-04 14:04:52 +0500

    1

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const header = document.getElementById('sticky');
 let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY < lastScrollY) {
-    // Scrolling up
-    header.classList.remove('hide');
-  } else {
-    // Scrolling down
-    header.classList.add('hide');
-  }
-  lastScrollY = window.scrollY;
+    if (window.scrollY < lastScrollY) {
+        // Scrolling up
+        header.classList.remove('hide');
+    } else {
+        // Scrolling down
+        header.classList.add('hide');
+    }
+    lastScrollY = window.scrollY;
 });
