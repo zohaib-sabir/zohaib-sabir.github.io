90f771d050f5af0489ee310f7771662dbf544bd3
Zohaib Sabir <zohaib.zaibi.pro@gmail.com>
2025-08-06 22:22:52 +0500

    1

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ window.addEventListener('scroll', () => {
     }
     lastScrollY = window.scrollY;
 });
+
+  window.addEventListener("load", function () {
+    const loader = document.getElementById("loader-wrapper");
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500); // Matches the transition duration in CSS
+  });
